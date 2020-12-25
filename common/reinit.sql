33af7fa23576f9f1d2fb27f4de5d31a97e48615a
Ivan Skovpin <ivanskovpin@gmail.com>
2020-12-25 17:06:38 +0700

    * kill all active sessions before dropping the DB

--- common/reinit.sql
+++ common/reinit.sql
@@ -1,2 +1,8 @@
+-- Kill all active sessions
+SELECT pg_terminate_backend(pg_stat_activity.pid)
+FROM pg_stat_activity
+WHERE pg_stat_activity.datname = 'dive_into_dbms';
+
+-- Recreate database
 drop database dive_into_dbms;
 create database dive_into_dbms;
