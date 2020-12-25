d48ca79dba05da2a0c4fae5c623fe693fc0731de
Ivan Skovpin <ivanskovpin@gmail.com>
2020-12-25 17:08:05 +0700

    + initial data for homework 3 task 2

---
+++ HW3Task2/init/db-structure.sql
@@ -0,0 +1,22 @@
+CREATE TABLE Planet
+(
+    id       SERIAL PRIMARY KEY,
+    name     TEXT UNIQUE,
+    distance NUMERIC(5, 2),
+    galaxy   INT CHECK (galaxy > 0)
+);
+
+CREATE TABLE Commander
+(
+    id   SERIAL PRIMARY KEY,
+    name TEXT
+);
+
+CREATE TABLE Flight
+(
+    id           INT PRIMARY KEY,
+    planet_id    INT REFERENCES Planet,
+    commander_id INT REFERENCES Commander,
+    start_date   DATE,
+    UNIQUE (commander_id, start_date)
+);
