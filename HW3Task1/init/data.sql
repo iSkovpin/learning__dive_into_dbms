34eaa1e4f9a1ab0f08b44282aa2ed2ea36997365
Ivan Skovpin <ivanskovpin@gmail.com>
2020-12-25 17:07:23 +0700

    + initial data for homework 3 task 1

---
+++ HW3Task1/init/data.sql
@@ -0,0 +1,36 @@
+INSERT INTO commander
+VALUES (1, 'John');
+INSERT INTO commander
+VALUES (2, 'Peter');
+
+INSERT INTO planet
+VALUES (1, 'Earth', 100);
+INSERT INTO planet
+VALUES (2, 'Mars', 200);
+INSERT INTO planet
+VALUES (3, 'Venus', 344);
+INSERT INTO planet
+VALUES (4, 'Pluto', 22);
+
+INSERT INTO spacecraft
+VALUES (1, 'Arrow', 33, 3030);
+INSERT INTO spacecraft
+VALUES (2, 'Blade', 22, 3031);
+INSERT INTO spacecraft
+VALUES (3, 'Destroyer', 99, 3001);
+
+INSERT INTO flight
+VALUES (1, 1, 1, 1, '2030-05-11');
+INSERT INTO flight
+VALUES (2, 1, 2, 1, '2030-05-12');
+INSERT INTO flight
+VALUES (3, 2, 3, 1, '2030-05-13');
+INSERT INTO flight
+VALUES (4, 3, 4, 2, '2030-07-11');
+INSERT INTO flight
+VALUES (5, 3, 3, 2, '2030-08-11');
+INSERT INTO flight
+VALUES (6, 3, 2, 1, '2030-09-11');
+INSERT INTO flight
+VALUES (7, 1, 1, 2, '2030-10-11');
+
