c1c1e8e455e884a8bd055358f25faae9d167fe87
Ivan Skovpin <ivanskovpin@gmail.com>
2021-02-15 23:25:20 +0700

    + homework 6 task 1 initial data

---
+++ HW6Task1/init/db-structure+data.sql
@@ -0,0 +1,11 @@
+CREATE TABLE Cities
+(
+    id    INT PRIMARY KEY,
+    value TEXT
+);
+-- Пример данных
+INSERT INTO Cities(id, value)
+VALUES (1, 'Воркута'),
+       (0, 'Джамбул'),
+       (2, 'Львов'),
+       (3, 'Азов');
